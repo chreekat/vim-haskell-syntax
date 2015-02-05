d2c27b56c9c683abd2e154552fb96cd6a5653c2b
Bryan Richter <b@chreekat.net>
2015-02-05 13:49:57 +0100

    Create really actually temp directories

--- test/runner/Main.hs
+++ test/runner/Main.hs
@@ -6,12 +6,19 @@ import Control.Applicative
 import Control.Monad
 
 import Data.Foldable
+import Data.List
+import Data.Monoid
 
 import System.FilePath
 import System.Directory
 import System.Process
 
 import Text.XML.HXT.Core
+
+-- | Minor convenience
+(<$/>) :: Functor f => f FilePath -> FilePath -> f FilePath
+fa <$/> b = (</> b) <$> fa
+infixl 4 <$/>
 
 main = join (Gold.defaultMain <$> goldenTests)
 
@@ -24,14 +31,30 @@ goldenTest inp = goldenVsFile inp (inp <.> ".gold.html") (inp <.> ".html") (genH
 goldenTests :: IO TestTree
 goldenTests = (testGroup "Usual Gold") <$> (map goldenTest) <$> goldenInputs
 
-tmp = "/tmp/vim-haskell-syntax-test-XXXXXX"
+openTempDirectory :: Maybe FilePath -- ^ optional location
+                  -> String -- ^ template
+                  -> IO FilePath
+openTempDirectory mdir template = do
+    nameString <- readProcess "mktemp"
+        ([ "-d" ]
+         <> (maybe [] (\dir -> ["--tmpdir="<>dir]) mdir)
+         <> [template])
+        ""
+    return $ chop nameString
+
+    where
+    chop = reverse . (!! 1) . tails . reverse
+
+tmp = do
+    tmpdir <- Just <$> getTemporaryDirectory
+    openTempDirectory tmpdir "vim-haskell-syntax-test-XXXXX"
 
 -- | Generates <input>.html from <input>, using vim.
 --
 -- Also uses a tmp directory to dump intermediate files.
 genHtml :: FilePath -> IO ()
 genHtml input = do
-    let target = tmp </> (takeFileName input)
+    target <- tmp <$/> (takeFileName input)
     copyFile input target
     callProcess "vim"
         [ "-E"
@@ -40,6 +63,7 @@ genHtml input = do
         , "--cmd", "view " ++ target
         ]
     void $ runX $ stripTitle (target <.> ".html") (input <.> ".html")
+    removeDirectoryRecursive (takeDirectory target)
 
 stripTitle :: FilePath -> FilePath -> IOSArrow XmlTree XmlTree
 stripTitle inp out =
